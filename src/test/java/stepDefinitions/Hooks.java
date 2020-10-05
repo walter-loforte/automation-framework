9d1af8566e73f586613d3085e2de44a1eec97600
walter-loforte <72238253+walter-loforte@users.noreply.github.com>
2020-10-05 18:25:59 -0300

    Remove commented code

--- src/test/java/stepDefinitions/Hooks.java
+++ src/test/java/stepDefinitions/Hooks.java
@@ -24,22 +24,6 @@ public class Hooks {
 	public static void setDriver(WebDriver driver) {
 		Hooks.driver = driver;
 	}
-//	private WebDriver createLocalDriver() {
-//        switch (driverType) {     
-//        case FIREFOX : driver = new FirefoxDriver();
-//     	break;
-//        case CHROME : 
-//        	System.setProperty(CHROME_DRIVER_PROPERTY, FileReaderManager.getInstance().getConfigReader().getDriverPath());
-//        	driver = new ChromeDriver();
-//     break;
-//        case INTERNETEXPLORER : driver = new InternetExplorerDriver();
-//     break;
-//        }
-// 
-//        if(FileReaderManager.getInstance().getConfigReader().getBrowserWindowSize()) driver.manage().window().maximize();
-//        driver.manage().timeouts().implicitlyWait(FileReaderManager.getInstance().getConfigReader().getImplicitlyWait(), TimeUnit.SECONDS);
-// return driver;
-// } 
 
 	@Before
 	public static void setupClass() {
